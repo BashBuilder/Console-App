bb6c24f89c58447d19224216503b4f89288c91e8
Bashbuilder <iamadelowotan@gmail.com>
2026-06-20 08:17:07 +0100

    feat: number types

--- Example.cs
+++ Example.cs
@@ -10,8 +10,12 @@ namespace ConsoleApp
       int a = 10;
       long b = 20L;
       double c = -30.5D;
+
+      int d = 121, e = 122, f = 123;
+      int g, h, i;
       Console.WriteLine("Hello, world");
-      Console.WriteLine($"a: {a}, b: {b}, c : {c}");
+      Console.WriteLine($"a: {a}, b: {b}, c : {c}, maxValue : {double.MaxValue} , minValue : {double.MinValue}");
+      Console.WriteLine($"d: {d}, e: {e}, f : {f}");
     }
   }
 }
