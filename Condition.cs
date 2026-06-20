3a61b1b19d2865575d60834e47368e93ab321cff
Bashbuilder <iamadelowotan@gmail.com>
2026-06-20 10:59:36 +0100

    feat: added dotnet watch config

---
+++ Condition.cs
@@ -0,0 +1,32 @@
+
+namespace ConsoleApp
+{
+  internal class Condition
+  {
+    static void Run()
+    {
+      int num = 10;
+
+      switch (num)
+      {
+        case 0:
+          Console.WriteLine("Zero");
+          break;
+        case 1:
+          Console.WriteLine("One"); break;
+        case 2:
+          Console.WriteLine("");
+          break;
+        default:
+          Console.WriteLine("Default case");
+          break;
+      }
+
+
+      Console.WriteLine("In the conditional example");
+      Console.WriteLine(num);
+    }
+  }
+
+}
+
--- Example.cs
+++ Example.cs
@@ -12,10 +12,46 @@ namespace ConsoleApp
       double c = -30.5D;
 
       int d = 121, e = 122, f = 123;
-      int g, h, i;
       Console.WriteLine("Hello, world");
       Console.WriteLine($"a: {a}, b: {b}, c : {c}, maxValue : {double.MaxValue} , minValue : {double.MinValue}");
       Console.WriteLine($"d: {d}, e: {e}, f : {f}");
+
+      Console.WriteLine($"{a + b} {a - b} {a * b} {a / b} {a % b} {a++} {++a} {a--} {--a}");
+
+      // string types
+      string name = "Anthony";
+      char initials = 'A';
+      Console.WriteLine($"My name is {name} and my initial is {initials}");
+
+      string firstName = Convert.ToString(Console.ReadLine()) ?? string.Empty;
+      string lastName = Convert.ToString(Console.ReadLine()) ?? string.Empty;
+
+      int ageInt = Convert.ToInt32(Console.ReadLine());
+
+      if (ageInt < 18)
+      {
+        Console.WriteLine("You are a minor");
+      }
+      else if (ageInt >= 18 && ageInt < 65)
+      {
+        Console.WriteLine("You are an adult");
+      }
+      else
+      {
+        Console.WriteLine("You are a senior citizen");
+      }
+
+      Console.WriteLine($"My name is {firstName} {lastName} and I am {ageInt} years old.");
+
+
+
+      // constants
+      const int num = 1000;
+      Console.WriteLine(num);
+
+      // num = 299;
+
+
     }
   }
 }
