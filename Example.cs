c17e8906dab68fd5089f0e8fc718fe465be1dfa4
Bashbuilder <iamadelowotan@gmail.com>
2026-06-20 08:13:07 +0100

    feat: initialized app

---
+++ Example.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+namespace ConsoleApp
+{
+  internal class Example
+  {
+    static void Main(string[] args)
+    {
+      int a = 10;
+      long b = 20L;
+      double c = -30.5D;
+      Console.WriteLine("Hello, world");
+      Console.WriteLine($"a: {a}, b: {b}, c : {c}");
+    }
+  }
+}
