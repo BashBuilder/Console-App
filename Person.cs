122a2a14dd01b9bc3fb281b515de166ef59299a5
Bashbuilder <iamadelowotan@gmail.com>
2026-06-27 07:13:22 +0100

    feat: class constructor initialized

---
+++ Person.cs
@@ -0,0 +1,25 @@
+class Person
+{
+  private string name;
+  private int age;
+
+  public string Name
+  {
+    get => name;
+    set => name = value;
+  }
+
+  public int Age
+  {
+    get => age;
+    set => age = value;
+  }
+
+  Person(string name, int age)
+  {
+    this.name = name;
+    this.age = age;
+  }
+
+
+}
